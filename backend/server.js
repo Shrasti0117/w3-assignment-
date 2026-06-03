621b6f45d8a3e6a181d3e36cccb912d08babf8bc
shrasti gupta <guptashrasti25@gmail.com>
2026-06-03 15:15:58 +0530

    add vercel.json

--- backend/server.js
+++ backend/server.js
@@ -13,12 +13,13 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
-    credentials: true,
-  })
-);
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://w3-assignment-jim7.vercel.app"
+  ],
+  credentials: true
+}));
 app.use(express.json({ limit: "8mb" }));
 app.use(morgan("dev"));
 
